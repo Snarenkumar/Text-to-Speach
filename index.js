335c8b5a7d28d28846b13748d72cc161aa9e5a9a
S Naren Kumar <vssnaren@gmail.com>
2025-01-20 20:54:53 +0530

    change the local server to production server

--- index.js
+++ index.js
@@ -58,9 +58,12 @@ app.post("/generate-audio", (req, res) => {
 });
 
 // Start the servers
-app.listen(process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3001, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+// app.listen(process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3001, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// });
+
+
+//!local server is turned off it is running on production server 
 app.get("/", (req, res) => {
     res.send("Welcome to the Text-to-Speech API. Use the POST /generate-audio endpoint to generate audio.");
 });
