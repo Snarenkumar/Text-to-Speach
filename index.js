2a9b978b045961a327fddfe55a27782a2e05678c
S Naren Kumar <vssnaren@gmail.com>
2025-01-25 16:25:52 +0530

    bought0

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/generate-audio", (req, res) => {
     }
 });
 
-// Start the servers
+// Start the servers with 3001 server 
 app.listen(process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3001, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
