af89720c36c5feafbd1fc8a823fb332d028619a8
S Naren Kumar <vssnaren@gmail.com>
2025-01-28 20:28:46 +0530

    bought0

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post("/generate-audio", (req, res) => {
                 return res.status(500).send("Error generating audio");
             }
 
-            // Log success and send the generated audio file  
+            // Log success and send the generated audio file  removed0
             // 
             console.log("Audio file generated successfully. Sending file:", filePath);
             res.sendFile(filePath, (err) => {
